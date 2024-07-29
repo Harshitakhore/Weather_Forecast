48829442624413e25e3370ec8f0f2d240a3cbc77
Sourabh Namdev <sourabh.namdev@kitaboo.com>
2024-07-29 13:02:48 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.vertx:vertx-core:4.5.8")
+    implementation("io.vertx:vertx-web:4.5.8")
+    implementation("io.vertx:vertx-web-client:4.5.8")
+    implementation ("io.vertx:vertx-config:4.4.0") // If you are using configuration management
+    implementation ("io.vertx:vertx-config-yaml:4.4.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation ("com.fasterxml.jackson.core:jackson-databind")
+    implementation ("com.squareup.retrofit2:retrofit:2.1.0")
+    implementation ("com.squareup.retrofit2:converter-jackson:2.1.0")
+
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
